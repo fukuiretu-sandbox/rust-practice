11cd5e61cfd6eeb98275ece24d568304571d309f
fukuiretu <fukuiretu@gmail.com>
2022-01-09 15:43:02 +0900

    Add trait sample

---
+++ trait_sample/src/geo/mod.rs
@@ -0,0 +1,36 @@
+pub trait Geometry {
+    fn area(&self) -> f64;
+    fn name(&self) -> &str {
+        return "Geometry";
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Rectangle {
+    pub width: u32,
+    pub height: u32,
+}
+
+impl Geometry for Rectangle {
+    fn area(&self) -> f64 {
+        self.width as f64 * self.height as f64
+    }
+    fn name(&self) -> &str {
+        return "Rectangle";
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Triangle {
+    pub bottom: u32,
+    pub height: u32,
+}
+
+impl Geometry for Triangle {
+    fn area(&self) -> f64 {
+        self.bottom as f64 * self.height as f64 * 0.5
+    }
+    fn name(&self) -> &str {
+        return "Triangle";
+    }
+}
---
+++ trait_sample/src/main.rs
@@ -0,0 +1,20 @@
+use crate::geo::Geometry;
+
+mod geo;
+
+fn main() {
+    let a = geo::Rectangle {
+        width: 10,
+        height: 20,
+    };
+    let b = geo::Triangle {
+        bottom: 20,
+        height: 5,
+    };
+
+    // dbg!(a);
+    // dbg!(b);
+
+    println!("{}, {}", a.name(), a.area());
+    println!("{}, {}", b.name(), b.area());
+}
