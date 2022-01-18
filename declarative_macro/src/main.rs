16159b2af0202e46bac9c4eb915d05955247fd69
fukuiretu <fukuiretu@gmail.com>
2022-01-19 00:01:42 +0900

    Add example

--- declarative_macro/src/main.rs
+++ declarative_macro/src/main.rs
@@ -3,6 +3,17 @@ macro_rules! foo {
         println!("{}", $x)
     };
 }
+
+macro_rules! array {
+    ($ ($x:expr), *) => {
+        [ $( $x), *]
+    };
+}
 fn main() {
     foo!(1 + 2);
+
+    let ar = array!(1, 2, 3, 4);
+    for v in ar.iter() {
+        println!("{}", v);
+    }
 }
